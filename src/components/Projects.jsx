becab501cbc64e70bbd095bf917f172799d295ee
Shamayal Syed <shamayalsyed9@gmail.com>
2023-07-03 12:54:41 -0400

    added projects component

---
+++ src/components/Projects.jsx
@@ -0,0 +1,17 @@
+// import "../../../styles/income.css"
+
+const Projects = () => {
+  
+  return (
+    <div>
+      <h1>
+        This is my projects page.
+      </h1>
+      <h4>
+        Here I will display all of my recent projects and their stack.
+      </h4>
+    </div>
+  )
+}
+
+export default Projects;
