e9ebe0fbef2361fa9a6c1431f24936c632f38177
Shamayal Syed <shamayalsyed9@gmail.com>
2023-07-03 12:50:42 -0400

    added contact component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,14 @@
+// import "../../../styles/income.css"
+
+const Contact = () => {
+  
+  return (
+    <div>
+      <h1>
+        This is my contact page.
+      </h1>
+    </div>
+  )
+}
+
+export default Contact;
