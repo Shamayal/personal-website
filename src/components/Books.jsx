79994d3919394cf76ee8a5c3f85727c7a42e9f67
Shamayal Syed <shamayalsyed9@gmail.com>
2023-07-05 23:41:53 -0400

    added books page

---
+++ src/components/Books.jsx
@@ -0,0 +1,14 @@
+// import "../../../styles/income.css"
+
+const Books = () => {
+  
+  return (
+    <div>
+      <h1>
+        This is my books page.
+      </h1>
+    </div>
+  )
+}
+
+export default Books;
