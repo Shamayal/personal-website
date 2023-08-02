2071e86a4c780059d16172c33485c59b0166f404
Shamayal Syed <shamayalsyed9@gmail.com>
2023-08-02 00:05:50 -0400

    added skills section description

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,6 +7,24 @@ const Skills = () => {
       <h1>
         This is my skills page.
       </h1>
+      <h4>
+        Languages: JavaScript, HTML, CSS, Ruby, Python
+      </h4>
+      <h4>
+        Databases: PostgreSQL
+      </h4>
+      <h4>
+        Frameworks/Libraries: NodeJS, ReactJS, Ruby on Rails, Bootstrap, Express, jQuery, SASS, Ajax, EJS
+      </h4>
+      <h4>
+        Design: Figma, Adobe Photoshop, Adobe Lightroom, Canva
+      </h4>
+      <h4>
+        Testing: Cypress, Jest, Mocha, Chai
+      </h4>
+      <h4>
+        Version Control: Git
+      </h4>
     </div>
   )
 }
