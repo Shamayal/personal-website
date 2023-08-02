bc8a25a954d0886dc330f6f51da28f9499d81ab7
Shamayal Syed <shamayalsyed9@gmail.com>
2023-08-02 00:04:25 -0400

    added about section information

---
+++ src/components/About.jsx
@@ -0,0 +1,33 @@
+// import "../../../styles/income.css"
+
+const About = () => {
+  
+  return (
+    <div>
+      <h1>
+        This is my about page.
+      </h1>
+      <h5>
+      👋 Hi, I'm Shamayal, a health science researcher turned full-stack web developer. I'm eager to merge my interest in coding with my passion to create impactful user experiences and make a meaningful difference in the world of technology. I'm currently continuing my learning journey and seeking my first role as a junior developer. I am excited to contribute my newfound expertise, analytical skills, and problem-solving abilities to develop innovative solutions.
+
+
+📚 I am Currently Learning: 
+- React and React Native
+
+Previously, I worked in a research lab dedicated to enhancing the user experience. This position instilled in me a deep understanding of the importance of creating user-friendly interfaces, optimizing website performance, and adhering to best practices in web accessibility standards. I can bring this perspective to my work as a web developer, as I believe technology can improve people's lives. 
+
+I have demonstrated a strong ability to quickly learn and adapt, as seen during my time in the insurance industry. The dynamic nature of the industry required me to rapidly grasp new concepts and adapt to evolving business environments. This experience has honed my agility and ability to learn new technologies, frameworks, and programming languages efficiently, which is valuable in the constantly evolving field of web development.
+
+My varied background has equipped me with the adaptability necessary to thrive in different environments and industries. I have successfully transitioned between diverse sectors, which has provided me with a well-rounded skill set. This adaptability enables me to quickly acclimate to new project requirements, collaborate effectively with cross-functional teams, and bring fresh insights to web development projects.
+
+💪 My strengths include problem-solving, analytical thinking, and being goal-oriented.
+
+🍰 In my free time, I enjoy baking pastries, and other confections which I document on my food blog.
+
+Please don't hesitate to contact me if you want to discuss anything on my profile. I'm open to having insightful conversations and learning through new interactions!
+      </h5>
+    </div>
+  )
+}
+
+export default About;
