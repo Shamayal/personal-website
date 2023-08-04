0d8374b9041b82ab79301eb43975e093311828e7
Shamayal Syed <shamayalsyed9@gmail.com>
2023-08-03 23:24:58 -0400

    added icons to homepage

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,6 +1,8 @@
 import Skills from "../components/Skills";
 import About from "../components/About";
 import TypewriterComponent from "../typewriter"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin , faMedium } from '@fortawesome/free-brands-svg-icons';
 
 const HomePage = () => {
 
@@ -16,13 +18,17 @@ const HomePage = () => {
       </h1>
 
       <TypewriterComponent />
-      
+
       <h5>
         View my 
         <a href='https://flowcv.com/resume/74ec7glopg' target="_blank" rel="noopener noreferrer">
         resume
         </a>
       </h5>
+
+      <FontAwesomeIcon icon={faLinkedin} />
+
+      <FontAwesomeIcon icon={faMedium} />
   
       <h5>
         linkedIn
