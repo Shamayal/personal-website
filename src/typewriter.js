7cc54a97d1cda9f2195d76b88d8b52db201bf7e8
Shamayal Syed <shamayalsyed9@gmail.com>
2023-08-19 22:22:25 -0400

    code formatting

--- src/typewriter.js
+++ src/typewriter.js
@@ -1,20 +1,26 @@
-import React from 'react';
-import Typewriter from 'typewriter-effect';
+import React from "react";
+import Typewriter from "typewriter-effect";
 
 const TypewriterComponent = () => {
   return (
     <h4>
       <Typewriter
         options={{
-          strings: ['Full-Stack Web Developer', 'Coding Instructor', 'Blogger', 'Baker', 'Life-Long Learner'],
+          strings: [
+            "Full-Stack Web Developer",
+            "Coding Instructor",
+            "Blogger",
+            "Baker",
+            "Life-Long Learner",
+          ],
           autoStart: true,
           loop: true,
-          delay: 50,           // Adjust the delay between each character (in milliseconds)
-          deleteSpeed: 40,     // Adjust the speed of characters being deleted (in milliseconds)
-          pauseFor: 2000,      // Adjust the pause time before starting the next string (in milliseconds)
-          cursor: '|',         // Change the cursor character (set to false to hide the cursor)
-          wrapperClassName: 'typewriter-wrapper', // Add a CSS class to the wrapper element
-          cursorClassName: 'typewriter-cursor',   // Add a CSS class to the cursor element
+          delay: 50, // Adjust the delay between each character (in milliseconds)
+          deleteSpeed: 40, // Adjust the speed of characters being deleted (in milliseconds)
+          pauseFor: 2000, // Adjust the pause time before starting the next string (in milliseconds)
+          cursor: "|", // Change the cursor character (set to false to hide the cursor)
+          wrapperClassName: "typewriter-wrapper", // Add a CSS class to the wrapper element
+          cursorClassName: "typewriter-cursor", // Add a CSS class to the cursor element
         }}
       />
     </h4>
