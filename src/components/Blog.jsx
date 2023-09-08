b95d8543de455d007b44c2894e2d5bc46a584e1e
Shamayal Syed <shamayalsyed9@gmail.com>
2023-09-08 14:02:52 -0400

    added blog component

---
+++ src/components/Blog.jsx
@@ -0,0 +1,26 @@
+import "../styles/About.css";
+
+const About = () => {
+  return (
+    <div>
+      <p className="poppins-400-font">SOMETHING</p>
+      <h1 className="poppins-800-font">Blog</h1>
+      <div className="about-container">
+        <div className="about-text">
+          
+          <p className="montserrat-400-font">
+            Previously, I worked in a research lab dedicated to enhancing the
+            user experience. This position instilled in me a deep understanding
+            of the importance of creating user-friendly interfaces, optimizing
+            website performance, and adhering to best practices in web
+            accessibility standards. I can bring this perspective to my work as
+            a web developer, as I believe technology can improve people's lives.
+          </p>
+        </div>
+        
+      </div>
+    </div>
+  );
+};
+
+export default About;
