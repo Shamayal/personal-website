b078008be814659b0db404a5c615651b623cff4e
Shamayal Syed <shamayalsyed9@gmail.com>
2023-11-01 22:41:23 -0400

    added books for each month

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -7,24 +7,40 @@ const Books = () => {
       <p>GET IN TOUCH</p>
       <h2>Book Reviews</h2>
       <p>Where I post my favourite book of the month</p>
-      <h3>January 2023</h3>
-      <p>The Psychology of Money: Timeless lessons on wealth, greed, and happiness</p>
-      <h3>February 2023</h3>
-      <p>Hana Khan Carries On</p>
-      <h3>March 2023</h3>
-      <p>The Unspoken Rules: Secrets to Starting Your Career Off Right</p>
-      <h3>April 2023</h3>
-      <p>The Inheritance Games</p>
-      <h3>May 2023</h3>
+
+      <h3>November 2023</h3>
       <p></p>
+
+      <h3>October 2023</h3>
+      <p>Lessons in Chemistry</p>
+
+      <h3>September 2023</h3>
+      <p>Girls That Invest</p>
+
+      <h3>August 2023</h3>
+      <p></p>
+
+      <h3>July 2022</h3>
+      <p>The Unspoken Rules: Secrets to Starting Your Career Off Right</p>
+
       <h3>June 2023</h3>
       <p></p>
-      <h3>July 2023</h3>
+
+      <h3>May 2023</h3>
       <p></p>
-      <h3>August 2023</h3>
-      <p>Girls That Invest</p>
-      <h3>September 2023</h3>
-      <p>Circe</p>
+
+      <h3>April 2023</h3>
+      <p></p>
+
+      <h3>March 2023</h3>
+      <p></p>
+
+      <h3>February 2022</h3>
+      <p>The Psychology of Money: Timeless lessons on wealth, greed, and happiness</p>
+      
+      <h3>January 2023</h3>
+      <p>Hana Khan Carries On</p>
+
     </div>
   )
 }
