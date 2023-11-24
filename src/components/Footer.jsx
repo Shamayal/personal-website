9a915d6a9e2ead42b8ec1257dd477093330bdcbb
Shamayal Syed <shamayalsyed9@gmail.com>
2023-11-23 22:52:38 -0500

    updated footer page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,8 +10,8 @@ const Footer = () => {
             <li><h5>Home</h5></li> 
             <li>Home</li>
             <li>About Me</li>
-            <li>My Projects</li>
             <li>My Resume</li>
+            <li>My Projects</li>
             <li>Contact Me</li>
           </ul>
         </div>
