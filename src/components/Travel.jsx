8702b6df6dd7967326912a317b9d3d3811f8c0c4
Shamayal Syed <shamayalsyed9@gmail.com>
2023-12-06 22:04:08 -0500

    added travel photography section

---
+++ src/components/Travel.jsx
@@ -0,0 +1,29 @@
+import "../styles/About.css";
+
+const Travel = () => {
+  return (
+    <div>
+      <p className="poppins-400-font">EXPLORE MY TRAVEL ADVENTURES</p>
+      <h1 className="poppins-800-font">Travel Photography</h1>
+      <div className="about-container">
+        <div className="about-text">
+          
+          <p className="montserrat-400-font">
+            Previously, I worked in a research lab dedicated to enhancing the
+            user experience. This position instilled in me a deep understanding
+            of the importance of creating user-friendly interfaces, optimizing
+            website performance, and adhering to best practices in web
+            accessibility standards. I can bring this perspective to my work as
+            a web developer, as I believe technology can improve people's lives.
+            talk about transition to coding
+            and future goals
+            add python project
+          </p>
+        </div>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Travel;
