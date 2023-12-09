363b8cf2b43faf55ea6f788e77e0585fb2fe9d6b
Shamayal Syed <shamayalsyed9@gmail.com>
2023-12-08 23:34:46 -0500

    about paragraph edits

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,25 +9,31 @@ const About = () => {
       <h1 className="poppins-800-font">About Me</h1>
       <div className="about-container">
         <div className="about-text">
-          <p>From choreography on a stage to lines of code on a web page, I love combining the worlds of logic and creative design to make eye-catching, accessible, and user-friendly websites and applications.
-            
-          Technology leads, society follows. The move towards increasing inclusivity and diversity in the industry through representation is of importance to me.
-            
-          I'm excited to make the leap and continue refining my skills with the right company. Drop me a line and/or your favourite restaurant in Toronto in the contact form below or check me out at:</p>
-
           <p className="montserrat-400-font">
-            Hello World! I'm Shamayal Syed, a full-stack web developer from Toronto, Canada. As a health science researcher turned full-stack web developer, I love combining the worlds of logic and to make 
-            
+            Hello World! I'm Shamayal Syed, a Toronto-based full-stack web developer. As a health science researcher in the field of making technology accessible for people with dementia, turned full-stack web developer, I am passionate about contributing my coding skills to merging logic and accessibility to create user-friendly websites and applications and impactful user experiences.
+
+
+            Currently seeking my first role as a junior developer, I bring analytical prowess and problem-solving abilities to develop innovative solutions.
+
+            With prior experience in a research lab focused on enhancing user experience, I understand the importance of user-friendly interfaces, optimized website performance, and adherence to web accessibility standards. I believe technology can improve lives.
+
+            Demonstrating quick learning and adaptability, my time in the insurance industry equipped me to efficiently grasp new technologies, frameworks, and programming languages. This agility is crucial in the evolving field of web development.
+
+            My diverse background fosters adaptability, allowing me to thrive in different environments. Successfully transitioning between sectors, I bring a well-rounded skill set, quick acclimation to new project requirements, effective collaboration with cross-functional teams, and fresh insights to web development projects.
+
+            I'm excited to make the leap and continue refining my skills with the right company. Send me a message in the contact form below or message me on LinkedIn!
+
             eager to merge my interest in coding with my passion to create
             impactful user experiences and make a meaningful difference in the
-            world of technology. I'm currently continuing my learning journey
+            world of technology. 
+            
+            I'm currently continuing my learning journey
             and seeking my first role as a junior developer. I am excited to
             contribute my newfound expertise, analytical skills, and
             problem-solving abilities to develop innovative solutions.
             make this shorter
           </p>
-          <p className="montserrat-400-font">
-            Shorten this paragraph
+          <p className="montserrat-400-font">            
             Previously, I worked in a research lab dedicated to enhancing the
             user experience. This position instilled in me a deep understanding
             of the importance of creating user-friendly interfaces, optimizing
@@ -54,7 +60,7 @@ const About = () => {
             web development projects.
           </p>
           <p className="montserrat-400-font">
-            In my free time, I enjoy baking pastries, and other confections which I document on my food blog, <a href="https://www.instagram.com/mazedarcreations/" target="_blank"><strong>Ma Ze Dar Creations</strong></a>. I'm also an amateur photographer, and like to write
+            In my free time, I enjoy baking pastries, and other confections which I document on my food blog, <a href="https://www.instagram.com/mazedarcreations/" target="_blank"><strong>Ma Ze Dar Creations</strong></a>. I'm also an amateur travel photographer, and I like to write for my blog.
           </p>
         </div>
         <div>
