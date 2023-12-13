ebb43cb77aab25c4f8b34d6e90f050d21c7471bc
Shamayal Syed <shamayalsyed9@gmail.com>
2023-12-12 22:37:44 -0500

    finalized footer styling and content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,22 +4,9 @@ const Footer = () => {
   return (
     <footer>
       <div className="footer-container">
-
-        <div className="footer-links">
-          <ul className="links montserrat-400-font">
-            <li><h5>Home</h5></li> 
-            <li>Home</li>
-            <li>About Me</li>
-            <li>My Resume</li>
-            <li>My Projects</li>
-            <li>Contact Me</li>
-          </ul>
-        </div>
-
-        <div className="footer-copyright montserrat-400-font">
-          <p>Personal Website Developed and Designed by Shamayal Syed © 2023</p>
-        </div>
-
+        <p className="montserrat-400-font">
+          <strong>Developed and Designed by Shamayal Syed 👩‍💻</strong>
+        </p>
       </div>
     </footer>
   );
