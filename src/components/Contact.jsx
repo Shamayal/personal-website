bcbf2b8d962d915157cbec5ef51592d52f660243
Shamayal Syed <shamayalsyed9@gmail.com>
2023-12-14 23:50:25 -0500

    added contact styling and form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,15 +1,69 @@
-// import "../../../styles/income.css"
-
-import Email from "../media/Email.png"
+import React, { useState } from "react";
+import "../styles/Contact.css";
+import Email from "../media/Email.png";
 
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, email, message);
+  };
+
   return (
     <div>
       <p className="poppins-400-font">GET IN TOUCH</p>
       <h1 className="poppins-800-font">Contact Me</h1>
+      <p>
+        <img src={Email} alt="Email" height="35px" />: shamayal.syed@outlook.com
+      </p>
+      <form>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
 
-      <div>
-        <form>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="message">Message</label>
+          <textarea
+            id="message"
+            name="message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+        </div>
+
+        <button type="submit" onClick={handleSubmit}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
+
+{
+  /* <form>
           <input />
           <label>Name: </label>
           <input type="hidden" name="form-name" value="contact" />
@@ -20,16 +74,5 @@ const Contact = () => {
           <label htmlFor="message">Message:</label>
           <textarea id="message" name="message" required></textarea>
           <button type="submit">Submit</button>
-        </form>
-      </div>
-      <div>
-        <p>blah blah contact me</p>
-        <p>
-        <img src={Email} alt="Email" height="35px"/>: shamayal.syed@outlook.com
-        </p>
-      </div>
-    </div>
-  );
-};
-
-export default Contact;
+        </form> */
+}
