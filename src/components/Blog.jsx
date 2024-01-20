ae0b8a7cc4d612e4a065078c7216ceebdc0c6195
Shamayal Syed <shamayalsyed9@gmail.com>
2024-01-20 17:41:26 -0500

    spacing for blog article boxes

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,77 +1,65 @@
 import "../styles/Blog.css";
 
+import article1 from "../media/article1.jpeg";
+import article2 from "../media/article2.jpeg";
+import article3 from "../media/article3.jpeg";
+import article4 from "../media/article4.jpeg";
+
+const articles = [
+  {
+    title: "Bootcamp vs. Traditional Education: My Experience and Insights",
+    date: "Oct 25, 2023",
+    link: "https://medium.com/@shamayal/bootcamp-vs-traditional-education-my-experience-and-insights-8648f07dcc7b",
+    image: article4,
+  },
+  {
+    title:
+      "Toronto Tech Trek: The Chronicles of My Elevate Festival Experience",
+    date: "Sep 30, 2023",
+    link: "https://medium.com/@shamayal/toronto-tech-trek-the-chronicles-of-my-elevate-festival-experience-51e358a22b73",
+    image: article3,
+  },
+  {
+    title:
+      "The Challenges and Triumphs of Learning to Code in a Bootcamp Setting",
+    date: "Jul 31, 2023",
+    link: "https://medium.com/@shamayal/the-challenges-and-triumphs-of-learning-to-code-in-a-bootcamp-setting-162ee7966c",
+    image: article2,
+  },
+  {
+    title:
+      "Navigating a Coding Bootcamp: 7 Shining Beacons of Advice to Guide you Through the Tempestuous Waters",
+    date: "Jul 25, 2023",
+    link: "https://medium.com/@shamayal/navigating-a-coding-bootcamp-7-shining-beacons-of-advice-to-guide-you-through-the-tempestuous-41050ae18272",
+    image: article1,
+  },
+];
+
 const Blog = () => {
   return (
     <div id="blog">
       <p className="poppins-400-font">READ MY ARTICLES</p>
       <h1 className="poppins-800-font">My Blog</h1>
 
-      <a
-        href="https://medium.com/@shamayal/bootcamp-vs-traditional-education-my-experience-and-insights-8648f07dcc7b"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <div className="blog-container">
-          <div className="blog-text">
-            <h2 className="poppins-600-font">
-              Bootcamp vs. Traditional Education: My Experience and Insights
-            </h2>
+      <div className="blog-articles">
+        {articles.map((article, index) => (
+          <a
+            key={index}
+            href={article.link}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="blog-container">
+              <div className="blog-text">
+                <img src={article.image} alt="" className="blog-img" />
+                <h2 className="poppins-600-font">{article.title}</h2>
+                <p className="montserrat-400-font">{article.date}</p>
+              </div>
+            </div>
+          </a>
+        ))}
+      </div>
 
-            <p className="montserrat-400-font">Oct 25, 2023</p>
-          </div>
-        </div>
-      </a>
-
-      <a
-        href="https://medium.com/@shamayal/toronto-tech-trek-the-chronicles-of-my-elevate-festival-experience-51e358a22b73"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <div className="blog-container">
-          <div className="blog-text">
-            <h2 className="poppins-600-font">
-              Toronto Tech Trek: The Chronicles of My Elevate Festival
-              Experience
-            </h2>
-
-            <p className="montserrat-400-font">Sep 30, 2023</p>
-          </div>
-        </div>
-      </a>
-
-      <a
-        href="https://medium.com/@shamayal/the-challenges-and-triumphs-of-learning-to-code-in-a-bootcamp-setting-162ee7966c"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <div className="blog-container">
-          <div className="blog-text">
-            <h2 className="poppins-600-font">
-              The Challenges and Triumphs of Learning to Code in a Bootcamp
-              Setting
-            </h2>
-
-            <p className="montserrat-400-font">Jul 31, 2023</p>
-          </div>
-        </div>
-      </a>
-
-      <a
-        href="https://medium.com/@shamayal/navigating-a-coding-bootcamp-7-shining-beacons-of-advice-to-guide-you-through-the-tempestuous-41050ae18272"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <div className="blog-container">
-          <div className="blog-text">
-            <h2 className="poppins-600-font">
-              Navigating a Coding Bootcamp: 7 Shining Beacons of Advice to Guide
-              you Through the Tempestuous Waters
-            </h2>
-
-            <p className="montserrat-400-font">Jul 25, 2023</p>
-          </div>
-        </div>
-      </a>
     </div>
   );
 };
