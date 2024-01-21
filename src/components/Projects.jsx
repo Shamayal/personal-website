a0e23f2b528a3e9b1be1c47ed85f2aa09f65eb6c
Shamayal Syed <shamayalsyed9@gmail.com>
2024-01-21 18:17:29 -0500

    changed techstack to an array and mapped it in a new div

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -15,7 +15,7 @@ const projectObj = [
     title: "Frog Finance",
     gif: frogFinanceGif,
     alt: "Frog Finance Demo",
-    techStack: "React, Node.js, PostgreSQL, Tailwind, CSS",
+    techStack: ["React", "Node.js", "PostgreSQL", "Tailwind", "CSS"],
     description:
       "A full-stack, multi-page web application that allows users to track their income, and expenses, create savings and debt goals, view stocks, and learn about personal finance.",
     link: "https://github.com/Shamayal/frog-finance",
@@ -24,7 +24,7 @@ const projectObj = [
     title: "PhotoLabs",
     gif: photoLabsGif,
     alt: "PhotoLabs Demo",
-    techStack: "React, Axios, JS, SCSS, PostgreSQL",
+    techStack: ["React", "Axios", "JS", "SCSS", "PostgreSQL"],
     description:
       "A React-based single-page application that allows users to view and like photos, and navigate through different photo categories.",
     link: "https://github.com/Shamayal/photolabs-starter",
@@ -33,8 +33,9 @@ const projectObj = [
     title: "Re-Furnish",
     gif: reFurnishGif,
     alt: "Re-Furnish Demo",
-    techStack:
-      "EJS, Node.js, Bootstrap, JS, Express, SASS, jQuery, Ajax, PostgreSQL",
+    techStack: [
+      "EJS", "Node.js", "Bootstrap", "JS", "Express", "SASS", "jQuery", "Ajax", "PostgreSQL",
+    ],
     description:
       "A full stack, multi-page web application that allows users to browse furniture, 'like' and save listings to their favourites, and email the seller. Sellers can post and delete items for sale, and mark items as 'sold'.",
     link: "https://github.com/Shamayal/refurnish",
@@ -43,7 +44,7 @@ const projectObj = [
     title: "Tweeter",
     gif: tweeterGif,
     alt: "Tweeter Demo",
-    techStack: "HTML, CSS, JS, jQuery, Ajax, Node.js, Express",
+    techStack: ["HTML", "CSS", "JS", "jQuery", "Ajax", "Node.js", "Express"],
     description:
       "A simple, single-page Twitter clone that allows users to write and post tweets with a character limit of 140.",
     link: "https://github.com/Shamayal/tweeter",
@@ -52,7 +53,7 @@ const projectObj = [
   //   title: "TinyApp",
   //   gif: tinyAppPic,
   //   alt: "TinyApp Demo",
-  //   techStack: "Express, EJS, Node.js",
+  //   techStack: ["Express", "EJS", "Node.js"],
   //   description:
   //     "A full-stack web application that allows users to shorten and save long URLs.",
   //   link: "https://github.com/Shamayal/tinyapp",
@@ -69,7 +70,7 @@ const Projects = () => {
       <p className="poppins-400-font">WHAT I'VE WORKED ON</p>
       <h1 className="poppins-800-font">My Projects</h1>
       <p className="montserrat-400-font">
-          Hover over any project to learn more!
+        Hover over any project to learn more!
       </p>
       <div className="project-portfolio">
         {projectObj.map((project, index) => (
@@ -89,10 +90,19 @@ const Projects = () => {
                   className="project-gif"
                 />
                 <div className="project-overlay">
-                  <p className="montserrat-400-font">{project.description}</p>
-                  <p className="montserrat-400-font">
-                    <i>Tech Stack: {project.techStack}</i>
+                  <p className="montserrat-400-font project-description">
+                    <strong>{project.description}</strong>
                   </p>
+                  <div className="tech-stack">
+                    {project.techStack.map((tech, index) => (
+                      <p
+                        key={index}
+                        className="montserrat-400-font tech-bubble"
+                      >
+                        {tech}
+                      </p>
+                    ))}
+                  </div>
                   <a
                     href={project.link}
                     target="_blank"
