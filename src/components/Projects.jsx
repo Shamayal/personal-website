46aac0a3676eb5f76e80950fc53b6fd353dd899c
Shamayal Syed <shamayalsyed9@gmail.com>
2024-01-21 13:03:06 -0500

    added map function for projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,10 +10,10 @@ import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-const project = [
+const projectObj = [
   {
     title: "Frog Finance",
-    gif: require("../media/FrogFinance.gif").default,
+    gif: frogFinanceGif,
     alt: "Frog Finance Demo",
     techStack: "React, Node.js, PostgreSQL, Tailwind, CSS",
     description:
@@ -22,41 +22,43 @@ const project = [
   },
   {
     title: "PhotoLabs",
-    gif: require("../media/PhotoLabs.gif").default,
+    gif: photoLabsGif,
     alt: "PhotoLabs Demo",
     techStack: "React, Axios, JS, SCSS, PostgreSQL",
-    description: 
+    description:
       "A React-based single-page application that allows users to view and like photos, and navigate through different photo categories.",
     link: "https://github.com/Shamayal/photolabs-starter",
   },
   {
     title: "Re-Furnish",
-    gif: require("../media/ReFurnish.gif").default,
+    gif: reFurnishGif,
     alt: "Re-Furnish Demo",
-    techStack: "EJS, Node.js, Bootstrap, JS, Express, SASS, jQuery, Ajax, PostgreSQL",
+    techStack:
+      "EJS, Node.js, Bootstrap, JS, Express, SASS, jQuery, Ajax, PostgreSQL",
     description:
       "A full stack, multi-page web application that allows users to browse furniture, 'like' and save listings to their favourites, and email the seller. Sellers can post and delete items for sale, and mark items as 'sold'.",
     link: "https://github.com/Shamayal/refurnish",
   },
   {
     title: "Tweeter",
-    gif: require("../media/Tweeter.gif").default,
+    gif: tweeterGif,
     alt: "Tweeter Demo",
     techStack: "HTML, CSS, JS, jQuery, Ajax, Node.js, Express",
     description:
       "A simple, single-page Twitter clone that allows users to write and post tweets with a character limit of 140.",
     link: "https://github.com/Shamayal/tweeter",
   },
-  {
-    title: "TinyApp",
-    gif: require("../media/TinyApp.png").default,
-    alt: "TinyApp Demo",
-    techStack: "Express, EJS, Node.js",
-    description:
-      "A full-stack web application that allows users to shorten and save long URLs.",
-    link: "https://github.com/Shamayal/tinyapp",
-  },
-]
+  // {
+  //   title: "TinyApp",
+  //   gif: tinyAppPic,
+  //   alt: "TinyApp Demo",
+  //   techStack: "Express, EJS, Node.js",
+  //   description:
+  //     "A full-stack web application that allows users to shorten and save long URLs.",
+  //   link: "https://github.com/Shamayal/tinyapp",
+  // },
+];
+
 const Projects = () => {
   useEffect(() => {
     AOS.init();
@@ -67,164 +69,32 @@ const Projects = () => {
       <p className="poppins-400-font">WHAT I'VE WORKED ON</p>
       <h1 className="poppins-800-font">My Projects</h1>
       <p className="montserrat-400-font">
-          Hover over any project to learn more!
-        </p>
-      <div
-        data-aos="fade-right"
-        data-aos-duration="2000"
-        className="projects-container"
-      >
-        
-        <div>
-          <h2 className="poppins-600-font">Frog Finance</h2>
-          <img
-            src={frogFinanceGif}
-            alt="Frog Finance Demo"
-            className="project-gif"
-          />
-        </div>
-        <div className="project-details">
-          <p className="montserrat-400-font">
-            <i>Tech Stack: React, Node.js, PostgreSQL, Tailwind, CSS</i>
-          </p>
-          <p className="montserrat-400-font">
-            A full-stack, multi-page web application that allows users to track
-            their income, and expenses, create savings and debt goals, view
-            stocks, and learn about personal finance.
-          </p>
-          <a
-            href="https://github.com/Shamayal/frog-finance"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <button className="github-button">View on GitHub</button>
-          </a>
-        </div>
-      </div>
+        Hover over any project to learn more!
+      </p>
 
-      <div
-        data-aos="fade-left"
-        data-aos-duration="2000"
-        className="projects-container"
-      >
-        <div>
-          <h2 className="poppins-600-font">PhotoLabs</h2>
-          <img
-            src={photoLabsGif}
-            alt="PhotoLabs Demo"
-            className="project-gif"
-          />
+      {projectObj.map((project, index) => (
+        <div
+          key={index}
+          data-aos={index % 2 === 0 ? "fade-right" : "fade-left"}
+          data-aos-duration="4000"
+          className="projects-container"
+        >
+          <div>
+            <h2 className="poppins-600-font">{project.title}</h2>
+            <img src={project.gif} alt={project.alt} className="project-gif" />
+          </div>
+          <div className="project-details">
+            <p className="montserrat-400-font">
+              <i>Tech Stack: {project.techStack}</i>
+            </p>
+            <p className="montserrat-400-font">{project.description}</p>
+            <a href={project.link} target="_blank" rel="noopener noreferrer">
+              <button className="github-button">View on GitHub</button>
+            </a>
+          </div>
         </div>
-        <div className="project-details">
-          <p className="montserrat-400-font">
-            <i>Tech Stack: React, Axios, JS, SCSS, PostgreSQL</i>
-          </p>
-          <p className="montserrat-400-font">
-            A React-based single-page application that allows users to view and
-            "like" photos, and navigate through different photo categories.
-          </p>
-          <a
-            href="https://github.com/Shamayal/photolabs-starter"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <button className="github-button">View on GitHub</button>
-          </a>
-        </div>
-      </div>
-
-      <div
-        data-aos="fade-right"
-        data-aos-duration="2000"
-        className="projects-container"
-      >
-        <div>
-          <h2 className="poppins-600-font">re-furnish</h2>
-          <img
-            src={reFurnishGif}
-            alt="re-furnish Demo"
-            className="project-gif"
-          />
-        </div>
-        <div className="project-details">
-          <p className="montserrat-400-font">
-            <i>
-              Tech Stack: EJS, Node.js, Bootstrap, JS, Express, SASS, jQuery,
-              Ajax, PostgreSQL
-            </i>
-          </p>
-          <p className="montserrat-400-font">
-            A full stack, multi-page web application that allows users to browse
-            furniture, "like" and save listings to their favourites, and email
-            the seller. Sellers can post and delete items for sale, and mark
-            items as "sold".
-          </p>
-          <a
-            href="https://github.com/Shamayal/refurnish"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <button className="github-button">View on GitHub</button>
-          </a>
-        </div>
-      </div>
-
-      <div
-        data-aos="fade-left"
-        data-aos-duration="2000"
-        className="projects-container"
-      >
-        <div>
-          <h2 className="poppins-600-font">Tweeter</h2>
-          <img src={tweeterGif} alt="Tweeter Demo" className="project-gif" />
-        </div>
-        <div className="project-details">
-          <p className="montserrat-400-font">
-            <i>Tech Stack: HTML, CSS, JS, jQuery, Ajax, Node.js, Express</i>
-          </p>
-          <p className="montserrat-400-font">
-            A simple, single-page Twitter clone that allows users to write and
-            post tweets with a character limit of 140.
-          </p>
-          <a
-            href="https://github.com/Shamayal/tweeter"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <button className="github-button">View on GitHub</button>
-          </a>
-        </div>
-      </div>
-
-      <div className="projects-container">
-        <div>
-          <h2 className="poppins-600-font">TinyApp</h2>
-          <img
-            src={tinyAppPic}
-            alt="TinyApp Demo"
-            className="project-pic"
-            height="300px"
-          />
-        </div>
-        <div className="project-details">
-          <p className="montserrat-400-font">
-            <i>Tech Stack: Express, EJS, Node.js</i>
-          </p>
-          <p className="montserrat-400-font">
-            A full-stack web application that allows users to shorten and save
-            long URLs.
-          </p>
-          <a
-            href="https://github.com/Shamayal/tinyapp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <button className="github-button">View on GitHub</button>
-          </a>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
-
 export default Projects;
