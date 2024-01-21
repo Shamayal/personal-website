5c20d7396f9de4fac9b5b728c4c9edb58f88c89d
Shamayal Syed <shamayalsyed9@gmail.com>
2024-01-21 18:46:20 -0500

    updated about me paragraphs with new information

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,32 +9,33 @@ const About = () => {
       <h1 className="poppins-800-font">About Me</h1>
       <div className="about-container">
         <div className="about-text">
-          <p className="montserrat-400-font">
+          <p className="montserrat-600-font">
             Hello World! I'm Shamayal Syed, a Toronto-based full-stack web
-            developer. Leveraging my background as a health science researcher,
-            where my focus was on making technology accessible to people with
-            dementia, I'm passionate about merging my coding skills with web
-            accessibility principles to create impactful, user-friendly
-            experiences across websites and applications.
+            developer. My journey began as a student researcher at the Toronto
+            Rehabilitation Institute, where I focused on making technology
+            accessible to people with dementia, I'm passionate about merging my
+            coding skills with web accessibility principles to create impactful,
+            user-friendly experiences across websites and applications.
           </p>
-          <p className="montserrat-400-font">
-            My experience in the dynamic insurance industry honed my ability to
-            quickly grasp new technologies, frameworks, and programming
-            languages – a crucial skill in the evolving field of web development.
-            My diverse background fosters adaptability, enabling me to thrive in
-            different environments. Successfully transitioning between sectors,
-            I bring a well-rounded skill set, quick acclimation to new project
-            requirements, effective collaboration with cross-functional teams,
-            and fresh insights to web development projects.
+          <p className="montserrat-600-font">
+            Drawing from my experience in the dynamic insurance industry honed
+            my ability to quickly grasp new technologies, frameworks, and
+            programming languages – a crucial skill in the evolving field of web
+            development. My diverse background fosters adaptability, enabling me
+            to thrive in different environments. Successfully transitioning
+            between sectors, I bring a well-rounded skill set, quick acclimation
+            to new project requirements, effective collaboration with
+            cross-functional teams, and fresh insights to web development
+            projects.
           </p>
-          <p className="montserrat-400-font">
-            Eager to apply my analytical prowess and problem-solving abilities
-            to develop innovative solutions, I'm embarking on my journey into a
-            junior developer role. I'm actively seeking the right company to
-            further refine my coding skills. If you'd like to connect, feel free
-            to send me a message on LinkedIn or use the contact form below!
+          <p className="montserrat-600-font">
+            As a junior developer, I apply my analytical skills and
+            problem-solving abilities to create innovative solutions. I am
+            actively committed to advancing my skills and expanding my knowledge
+            on my career in software development.
           </p>
-          <p className="montserrat-400-font">
+
+          <p className="montserrat-600-font">
             In my free time, I indulge in baking pastries, and other confections
             which I document on my food blog,{" "}
             <a
