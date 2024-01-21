d6c6d6173fbcc013f40f22877354210233bf831e
Shamayal Syed <shamayalsyed9@gmail.com>
2024-01-21 12:27:09 -0500

    created project object

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,13 +5,59 @@ import reFurnishGif from "../media/ReFurnish.gif";
 import tweeterGif from "../media/Tweeter.gif";
 import tinyAppPic from "../media/TinyApp.png";
 
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-// add hover feature, modify this for mobile, maybe a tap feature
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+const project = [
+  {
+    title: "Frog Finance",
+    gif: require("../media/FrogFinance.gif").default,
+    alt: "Frog Finance Demo",
+    techStack: "React, Node.js, PostgreSQL, Tailwind, CSS",
+    description:
+      "A full-stack, multi-page web application that allows users to track their income, and expenses, create savings and debt goals, view stocks, and learn about personal finance.",
+    link: "https://github.com/Shamayal/frog-finance",
+  },
+  {
+    title: "PhotoLabs",
+    gif: require("../media/PhotoLabs.gif").default,
+    alt: "PhotoLabs Demo",
+    techStack: "React, Axios, JS, SCSS, PostgreSQL",
+    description: 
+      "A React-based single-page application that allows users to view and like photos, and navigate through different photo categories.",
+    link: "https://github.com/Shamayal/photolabs-starter",
+  },
+  {
+    title: "Re-Furnish",
+    gif: require("../media/ReFurnish.gif").default,
+    alt: "Re-Furnish Demo",
+    techStack: "EJS, Node.js, Bootstrap, JS, Express, SASS, jQuery, Ajax, PostgreSQL",
+    description:
+      "A full stack, multi-page web application that allows users to browse furniture, 'like' and save listings to their favourites, and email the seller. Sellers can post and delete items for sale, and mark items as 'sold'.",
+    link: "https://github.com/Shamayal/refurnish",
+  },
+  {
+    title: "Tweeter",
+    gif: require("../media/Tweeter.gif").default,
+    alt: "Tweeter Demo",
+    techStack: "HTML, CSS, JS, jQuery, Ajax, Node.js, Express",
+    description:
+      "A simple, single-page Twitter clone that allows users to write and post tweets with a character limit of 140.",
+    link: "https://github.com/Shamayal/tweeter",
+  },
+  {
+    title: "TinyApp",
+    gif: require("../media/TinyApp.png").default,
+    alt: "TinyApp Demo",
+    techStack: "Express, EJS, Node.js",
+    description:
+      "A full-stack web application that allows users to shorten and save long URLs.",
+    link: "https://github.com/Shamayal/tinyapp",
+  },
+]
 const Projects = () => {
-
   useEffect(() => {
     AOS.init();
   }, []);
@@ -20,10 +66,15 @@ const Projects = () => {
     <div id="projects">
       <p className="poppins-400-font">WHAT I'VE WORKED ON</p>
       <h1 className="poppins-800-font">My Projects</h1>
-
-      <p>hover over any project to learn more!</p>
-
-      <div data-aos="fade-right" data-aos-duration="2000" className="projects-container">
+      <p className="montserrat-400-font">
+          Hover over any project to learn more!
+        </p>
+      <div
+        data-aos="fade-right"
+        data-aos-duration="2000"
+        className="projects-container"
+      >
+        
         <div>
           <h2 className="poppins-600-font">Frog Finance</h2>
           <img
@@ -51,7 +102,11 @@ const Projects = () => {
         </div>
       </div>
 
-      <div data-aos="fade-left" data-aos-duration="2000" className="projects-container">
+      <div
+        data-aos="fade-left"
+        data-aos-duration="2000"
+        className="projects-container"
+      >
         <div>
           <h2 className="poppins-600-font">PhotoLabs</h2>
           <img
@@ -78,7 +133,11 @@ const Projects = () => {
         </div>
       </div>
 
-      <div data-aos="fade-right" data-aos-duration="2000" className="projects-container">
+      <div
+        data-aos="fade-right"
+        data-aos-duration="2000"
+        className="projects-container"
+      >
         <div>
           <h2 className="poppins-600-font">re-furnish</h2>
           <img
@@ -110,7 +169,11 @@ const Projects = () => {
         </div>
       </div>
 
-      <div data-aos="fade-left" data-aos-duration="2000" className="projects-container">
+      <div
+        data-aos="fade-left"
+        data-aos-duration="2000"
+        className="projects-container"
+      >
         <div>
           <h2 className="poppins-600-font">Tweeter</h2>
           <img src={tweeterGif} alt="Tweeter Demo" className="project-gif" />
@@ -133,7 +196,7 @@ const Projects = () => {
         </div>
       </div>
 
-      {/* <div className="projects-container">
+      <div className="projects-container">
         <div>
           <h2 className="poppins-600-font">TinyApp</h2>
           <img
@@ -159,7 +222,7 @@ const Projects = () => {
             <button className="github-button">View on GitHub</button>
           </a>
         </div>
-      </div> */}
+      </div>
     </div>
   );
 };
