0c935b97189dcf95e6695e260b5e21486f5cb6d5
Shamayal Syed <shamayalsyed9@gmail.com>
2024-01-22 16:20:04 -0500

    updated about section paragraphs

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,23 +11,20 @@ const About = () => {
       <div className="about-container">
         <div className="about-text">
           <p className="montserrat-600-font">
-            Hello World! I'm Shamayal Syed, a Toronto-based software developer.
-            My journey began as a student researcher at the Toronto
-            Rehabilitation Institute, where I focused on making technology
-            accessible to people with dementia, I'm passionate about merging my
-            coding skills with web accessibility principles to create impactful,
-            user-friendly experiences across websites and applications.
+            Hello World! I'm Shamayal Syed, a Toronto-based software developer
+            with a passion for creating impactful user-friendly experiences
+            through coding and accessibility. My journey began as a qualitative
+            researcher at the Toronto Rehabilitation Institute, where I delved
+            into making technology accessible to people with dementia.
           </p>
           <p className="montserrat-600-font">
-            My experience in the dynamic insurance industry honed my ability to
-            quickly grasp new technologies, frameworks, and programming
-            languages – a crucial skill in the evolving field of web
-            development. My diverse background in successfully transitioning
-            between sections fosters adaptability, enabling me to thrive in
-            different environments.
+            My diverse background includes a short period working in the dynamic
+            insurance industry which honed my ability to quickly grasp new
+            technologies, frameworks, and programming languages – a crucial
+            skill in the evolving field of web development.
           </p>
           <p className="montserrat-600-font">
-            As a junior developer, I apply my analytical skills and
+            Now as a junior developer, I apply my analytical skills and
             problem-solving abilities to create innovative solutions. I am
             actively committed to advancing my skills and expanding my knowledge
             on my career in software development.
@@ -44,9 +41,13 @@ const About = () => {
                 <strong>Ma Ze Dar Creations</strong>
               </i>
             </a>
-            . I'm also an amateur travel photographer, avid reader (feel free to
-            reach out to me with any book recommendations!) and sporadic blogger
-            chronicling my coding journey.
+            . I'm also an amateur travel photographer, avid reader, and sporadic
+            blogger chronicling my coding journey.
+          </p>
+          <p className="montserrat-600-font">
+            If you're curious to learn more about my coding journey, explore my
+            portfolio or connect with me on social media. I'm always open to
+            book recommendations and conversations!
           </p>
         </div>
         <div>
