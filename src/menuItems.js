b1b61f99dc43a20df3936a3454f9d8b72930da85
Shamayal Syed <shamayalsyed9@gmail.com>
2026-01-01 23:26:17 -0500

    Commented out additional menu items

--- src/menuItems.js
+++ src/menuItems.js
@@ -15,33 +15,33 @@ export const menuItems = [
     title: "Blog",
     url: "/blog",
   },
-  {
-    title: "Book Reviews",
-    url: "/books"
-  },
   // {
-  //   title: "Photography", // need to fix, this is bold for some reason
-  //   url: "",
-  //   submenu: [
-  //     {
-  //       title: "Photography",
-  //       submenu: [
-  //         {
-  //           title: "Ma Ze Dar Creations",
-  //           url: "/hobbies/photography/food",
-  //         },
-  //         {
-  //           title: "Travel",
-  //           url: "/hobbies/photography/travel",
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       title: "Book Reviews",
-  //       url: "/hobbies/books",
-  //     },
-  //   ],
+  //   title: "Book Reviews",
+  //   url: "/books"
   // },
+  // {
+  // title: "Photography", // need to fix, this is bold for some reason
+  // url: "/photography"
+  // },
+    // submenu: [
+    //   {
+    //     title: "Photography",
+    //     submenu: [
+    //       {
+    //         title: "Ma Ze Dar Creations",
+    //         url: "/hobbies/photography/food",
+    //       },
+    //       {
+    //         title: "Travel",
+    //         url: "/hobbies/photography/travel",
+    //       },
+    //     ],
+    //   },
+    //   {
+    //     title: "Book Reviews",
+    //     url: "/hobbies/books",
+    //   },
+    // ],
   {
     title: "Contact",
     url: "/#contact",
